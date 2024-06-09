88923e5c04892fa47ea1e9e3074f9807ae04e856
Grace <graceofubu04@gmail.com>
2024-06-09 00:48:31 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Cart from "./components/Cart";
-import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>hi</div>;
 }
 
 export default App;
